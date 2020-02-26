316b978096917280bcdd120a9df2126cdb45eb50
krm-shrftdnv <42980681+krm-shrftdnv@users.noreply.github.com>
2020-02-27 00:48:29 +0300

    First homework added.
    So late...

---
+++ 1/index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<form action="hello.php" method="post">
+    Введите код мозготрахаря: <textarea name="code"></textarea><br/>
+    Введите входные параметры: <input type="text" name="input">
+    <input type="submit" value="Нажмите кнопку, чтобы запустить говнокод!">
+</form>
+<?php
+
+?>
+</body>
+</html>
