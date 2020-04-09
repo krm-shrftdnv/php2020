0ec3273d91e742f2dacfba6bba52a08be0ac0bd9
krm-shrftdnv <42980681+krm-shrftdnv@users.noreply.github.com>
2020-04-09 23:08:03 +0300

    7th wonder of the homework

---
+++ 7/index.php
@@ -0,0 +1,45 @@
+<?php
+include("form7.html");
+set_time_limit(0);
+if(!isset ($_REQUEST['link'])) return;
+$link = escapeshellarg($_REQUEST['link']);
+$type = $_REQUEST['type'];
+
+$message = "Проверьте имя узла и повторите попытку.";
+
+$arr = [];
+
+exec($type.' '.$link, $arr);
+
+mb_convert_variables("UTF-8","cp866", $arr);
+
+//foreach ($arr as $string) {
+//    echo $string;
+//    echo "<br>";
+//}
+
+if(sizeof($arr)<3) echo $message;
+else decorate($arr, $type);
+
+function decorate($strings, $type) {
+    $arr = [];
+    preg_match('/\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/', $strings[1], $arr);
+    $address = $arr[0];
+    echo "<b>".$address."</b><br>";
+    switch ($type) {
+        case 'ping' : {
+            preg_match('/[^0-9]/', $strings[9], $arr);
+            $loss = $arr[0];
+            echo "Successful requests - ".(100-(int)$loss)."%";
+            break;
+        }
+        case 'tracert' : {
+            echo "Jumps were to:<br>";
+            for ($i = 3; $i < sizeof($strings)-2; $i++) {
+                preg_match('/\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/', $strings[$i], $arr);
+                echo $arr[0]." ";
+            }
+            break;
+        }
+    }
+}
